60999b897b4901940226e0fb30118fd2e93f7ffa
Victor Parada <vhparadajr@gmail.com>
2023-05-10 23:09:29 -0400

    feat: added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import { Container } from "react-bootstrap"
+import { Outlet } from "react-router-dom"
+
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import { Outlet } from "react-router-dom"
 
 const App = () => {
   return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,17 +6,20 @@ import {
   Route,
   RouterProvider
  } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css'; 
+ import reportWebVitals from './reportWebVitals';
+
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './components/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen/>} />
+      <Route path="/product/:id" element={<ProductScreen/>} />
     </Route>
   )
 );
