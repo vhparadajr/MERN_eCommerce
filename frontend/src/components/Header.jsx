6d2b4438e20ac57776f0f965fb6c17fb18e348dd
Victor Parada <vhparadajr@gmail.com>
2023-05-10 23:10:02 -0400

    feat: connected routes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,21 +2,32 @@ import { Navbar, Nav, Container, NavLink } from 'react-bootstrap';
 import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="ProShop" width="30" height="30" className="d-inline-block align-top" />
-            ProShop
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand href="/">
+              <img src={logo} alt="ProShop" width="30" height="30" className="d-inline-block align-top" />
+              ProShop
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <NavbarCollapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <NavLink href="/cart"><FaShoppingCart /> Cart</NavLink>
-              <NavLink href="/login"><FaUser /> Sign In</NavLink>
+              <LinkContainer to="/cart">
+                <NavLink>
+                  <FaShoppingCart /> Cart
+                </NavLink>
+              </ LinkContainer>
+              <LinkContainer to="/login">
+                <NavLink>
+                  <FaUser /> Sign In
+                </NavLink>
+              </LinkContainer>
             </Nav>
           </NavbarCollapse>
         </Container>  
