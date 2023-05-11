1af67992183f72ba991324cfa274955c2b0ed62b
Victor Parada <vhparadajr@gmail.com>
2023-05-11 12:38:05 -0400

    feat: integrate client-side data fetching with server via Axios

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react"
 import { Col, Row } from "react-bootstrap" 
-import products from "../products"
+import axios from "axios"
+
 import Product from "../components/Product"
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("api/products");
+      console.log(data);
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
