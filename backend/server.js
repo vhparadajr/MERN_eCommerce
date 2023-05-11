f8f8a7865ef6b22c09ab04db501ad4fdc5e94828
Victor Parada <vhparadajr@gmail.com>
2023-05-11 16:00:16 -0400

    feat: added db resource and created schemas for orders, products, and users

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv, { config } from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
+
 import products from './data/products.js';
 
 const port = process.env.PORT || 6000;
 
+connectDB(); // Connect to MongoDB  
 const app = express();
 
 app.get('/', (req, res) => {
