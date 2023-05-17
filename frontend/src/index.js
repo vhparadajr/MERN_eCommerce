a5fe1f58f1702e736c5624bd6958a42a163c3d5c
Victor Parada <vhparadajr@gmail.com>
2023-05-17 14:34:06 -0400

    feat: added redux/redux-toolkit for state management

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import {
   RouterProvider
  } from 'react-router-dom';
  import reportWebVitals from './reportWebVitals';
+ import { Provider } from 'react-redux';
+ import store from './store';
 
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
@@ -27,7 +29,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
