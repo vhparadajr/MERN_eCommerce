82c39e30516e9572418f30ac9d2dc2a3b37294b7
Victor Parada <vhparadajr@gmail.com>
2023-05-17 11:06:18 -0400

    feat: express middleware and products route

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,7 @@ import express from 'express';
 import dotenv, { config } from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-
-import products from './data/products.js';
+import productRoutes from './routes/productsRoutes.js';
 
 const port = process.env.PORT || 6000;
 
@@ -14,12 +13,6 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  res.json(products.find(product => product._id === req.params.id));
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
