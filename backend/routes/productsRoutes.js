e4142ede27abb32254950262a4ed8bcbbe7f5495
Victor Parada <vhparadajr@gmail.com>
2023-05-17 12:51:21 -0400

    feat: added product controller and updated products route

--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -1,24 +1,10 @@
 import express from "express";
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from "../models/productModel.js";
+import { getProducts, getProductById } from '../controllers/productControllers.js';
 
 const router = express.Router();
 
-router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
-}));
-
-router.get('/:id', asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  
-  if(Product){
-    return res.json(product)
-  }
-
-  res.status(404);
-  throw new Error('Resource not found');
-}));
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 
 export default router;
