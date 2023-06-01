12f9478d913f74eeddccb3ae41174c81c71cd63e
Victor Parada <vhparadajr@gmail.com>
2023-06-01 14:11:14 -0400

    refactor: temp change to use local data vs DB

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,21 +1,24 @@
 import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
+// import Product from "../models/productModel.js";
+//Temp change to use local data
+import  Products from "../data/products.js";
 
 // @desc    Fetch all products
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => { 
-  const products = await Product.find({});
-  res.json(products);
+  // const products = await Product.find({});
+  res.json(Products);
 });
 
 // @desc    Fetch single product
 // @route   GET /api/products/:id
 // @access  Public
 const getProductById = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  // const product = await Product.findById(req.params.id);
+  const product = Products.find((product) => product._id === req.params.id);
   
-  if(Product){
+  if(product){
     return res.json(product)
   }
 
